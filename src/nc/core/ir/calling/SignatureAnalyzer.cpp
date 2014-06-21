df37552d51f6467df9de19aab423917410348d88
Yegor Derevenets <yegor.derevenets@gmail.com>
2014-06-21 20:11:02 +0200

    Use any symbol for function name if there is no symbol of function type

--- src/nc/core/ir/calling/SignatureAnalyzer.cpp
+++ src/nc/core/ir/calling/SignatureAnalyzer.cpp
@@ -618,7 +618,11 @@ void SignatureAnalyzer::computeName(const CalleeId &calleeId, FunctionSignature 
 
     if (calleeId.entryAddress()) {
         /* Take the name of the corresponding symbol, if possible. */
-        if (auto symbol = image_.symbols()->find(image::Symbol::Function, *calleeId.entryAddress())) {
+        auto symbol = image_.symbols()->find(image::Symbol::Function, *calleeId.entryAddress());
+        if (!symbol) {
+            symbol = image_.symbols()->find(image::Symbol::None, *calleeId.entryAddress());
+        }
+        if (symbol) {
             signature.setName(likec::Tree::cleanName(symbol->name()));
 
             if (signature.name() != symbol->name()) {
