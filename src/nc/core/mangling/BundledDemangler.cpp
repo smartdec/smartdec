af407999f968dac7afb0cbedab81b99fcb11593c
Yegor Derevenets <yegor.derevenets@gmail.com>
2014-06-22 14:28:31 +0200

    Fixed demangler search path

--- src/nc/core/mangling/BundledDemangler.cpp
+++ src/nc/core/mangling/BundledDemangler.cpp
@@ -16,8 +16,8 @@ namespace mangling {
 
 BundledDemangler::BundledDemangler() {
     QStringList locations;
-    locations << (QCoreApplication::applicationDirPath() + "demangler");
-    locations << (QCoreApplication::applicationDirPath() + "demangler.exe");
+    locations << (QCoreApplication::applicationDirPath() + "/demangler");
+    locations << (QCoreApplication::applicationDirPath() + "/demangler.exe");
     locations << (QCoreApplication::applicationDirPath() + "/../demangler/demangler");
     locations << (QCoreApplication::applicationDirPath() + "/../demangler/demangler.exe");
     
