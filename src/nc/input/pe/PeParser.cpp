03ee8284d4a82494dffe68d46243bae982b195e7
Yegor Derevenets <yegor.derevenets@gmail.com>
2014-07-08 19:56:26 +0200

    Cleanup in the PeParser

--- src/nc/input/pe/PeParser.cpp
+++ src/nc/input/pe/PeParser.cpp
@@ -30,6 +30,7 @@
 #include <nc/common/ByteOrder.h>
 #include <nc/common/Conversions.h>
 #include <nc/common/Foreach.h>
+#include <nc/common/Warnings.h>
 #include <nc/common/make_unique.h>
 
 #include <nc/core/image/BufferByteSource.h>
@@ -84,12 +85,9 @@ class PeParserPrivate {
     QIODevice *source_;
     core::image::Image *image_;
 
-    std::unique_ptr<char[]> stringTable_;
-    uint32_t stringTableSize_;
-
 public:
     PeParserPrivate(QIODevice *source, core::image::Image *image):
-        source_(source), image_(image), stringTableSize_(0)
+        source_(source), image_(image)
     {}
 
     void parse() {
@@ -109,99 +107,32 @@ public:
         switch (fileHeader.Machine) {
             case IMAGE_FILE_MACHINE_I386:
                 image_->setArchitecture(QLatin1String("i386"));
-                parseHeaders<IMAGE_OPTIONAL_HEADER32, IMAGE_NT_OPTIONAL_HDR32_MAGIC>(fileHeader);
                 break;
             case IMAGE_FILE_MACHINE_AMD64:
                 image_->setArchitecture(QLatin1String("x86-64"));
-                parseHeaders<IMAGE_OPTIONAL_HEADER64, IMAGE_NT_OPTIONAL_HDR64_MAGIC>(fileHeader);
                 break;
             default:
                 throw core::input::ParseError(tr("Unknown machine id: %1.").arg(fileHeader.Machine));
         }
 
-        if (fileHeader.PointerToSymbolTable && fileHeader.NumberOfSymbols) {
-            if (!source_->seek(fileHeader.PointerToSymbolTable)) {
-                throw core::input::ParseError(tr("Cannot seek to the symbol table."));
-            }
-
-            /*
-             * http://www.delorie.com/djgpp/doc/coff/symtab.html
-             */
-            std::vector<IMAGE_SYMBOL> symbols(fileHeader.NumberOfSymbols);
-
-            if (source_->read(reinterpret_cast<char *>(&symbols[0]), sizeof(IMAGE_SYMBOL) * fileHeader.NumberOfSymbols) !=
-                static_cast<qint64>(sizeof(IMAGE_SYMBOL) * fileHeader.NumberOfSymbols))
-            {
-                throw core::input::ParseError(tr("Cannot read the symbol table."));
-            }
-
-            if (source_->read(reinterpret_cast<char *>(&stringTableSize_), sizeof(stringTableSize_)) != sizeof(stringTableSize_)) {
-                throw core::input::ParseError(tr("Cannot read the size of string table."));
-            }
-
-            stringTable_.reset(new char[stringTableSize_]);
-            memset(stringTable_.get(), 0, 4);
-            if (source_->read(stringTable_.get() + 4, stringTableSize_ - 4) != stringTableSize_ - 4) {
-                throw core::input::ParseError(tr("Cannot read the string table."));
-            }
-
-            foreach (IMAGE_SYMBOL &symbol, symbols) {
-                peByteOrder.convertFrom(symbol.Type);
-                peByteOrder.convertFrom(symbol.Value);
-
-                using core::image::Symbol;
-
-                Symbol::Type type;
-                /*
-                 * Microsoft tools set this field to 0x20 (function) or 0x0 (not a function).
-                 * -- http://www.kishorekumar.net/pecoff_v8.1.htm
-                 */
-                if (symbol.Type == 0x20) {
-                    type = Symbol::FUNCTION;
-                } else {
-                    type = Symbol::NOTYPE;
-                }
-
-                QString name;
-                if (symbol.N.Name.Short) {
-                    name = getString(symbol.N.ShortName);
-                } else {
-                    name = getStringFromTable(symbol.N.Name.Long);
-                }
-
-                image_->addSymbol(std::make_unique<Symbol>(type, name, symbol.Value));
-            }
-
-            foreach (auto section, image_->sections()) {
-                if (section->name().startsWith('/')) {
-                    uint32_t offset;
-                    if (stringToInt(section->name().mid(1), &offset)) {
-                        QString newName = getStringFromTable(offset);
-                        if (!newName.isEmpty()) {
-                            section->setName(newName);
-                        }
-                    }
-                }
-            }
-        }
+        parseSections(fileHeader);
+        parseSymbols(fileHeader);
     }
 
-    template<class OptionalHeader, WORD OptionalHeaderMagic>
-    void parseHeaders(const IMAGE_FILE_HEADER &fileHeader) {
-        OptionalHeader optionalHeader;
-        if (source_->read(reinterpret_cast<char *>(&optionalHeader), sizeof(optionalHeader)) != sizeof(optionalHeader)) {
-            throw core::input::ParseError(tr("Cannot read the optional header."));
-        }
-        peByteOrder.convertFrom(optionalHeader.Magic);
-        if (optionalHeader.Magic != OptionalHeaderMagic) {
-            throw core::input::ParseError(tr("Magic of the optional header doesn't match."));
+private:
+    void parseSections(const IMAGE_FILE_HEADER &fileHeader) {
+        if (!source_->seek(source_->pos() + fileHeader.SizeOfOptionalHeader)) {
+            ncWarning("Cannot seek to the section header table.");
+            return;
         }
 
         for (std::size_t i = 0; i < fileHeader.NumberOfSections; ++i) {
             IMAGE_SECTION_HEADER sectionHeader;
             if (source_->read(reinterpret_cast<char *>(&sectionHeader), sizeof(sectionHeader)) != sizeof(sectionHeader)) {
-                throw core::input::ParseError(tr("Cannot read section header #%1.").arg(i));
+                ncWarning("Cannot read the section header %1.", i);
+                return;
             }
+
             peByteOrder.convertFrom(sectionHeader.VirtualAddress);
             peByteOrder.convertFrom(sectionHeader.SizeOfRawData);
             peByteOrder.convertFrom(sectionHeader.Characteristics);
@@ -233,18 +164,93 @@ public:
         }
     }
 
-private:
+    void parseSymbols(const IMAGE_FILE_HEADER &fileHeader) {
+        if (!fileHeader.PointerToSymbolTable || !fileHeader.NumberOfSymbols) {
+            return;
+        }
+
+        if (!source_->seek(fileHeader.PointerToSymbolTable)) {
+            ncWarning("Cannot seek to the symbol table.");
+            return;
+        }
+
+        /*
+         * http://www.delorie.com/djgpp/doc/coff/symtab.html
+         */
+        std::vector<IMAGE_SYMBOL> symbols(fileHeader.NumberOfSymbols);
+
+        if (source_->read(reinterpret_cast<char *>(&symbols[0]), sizeof(IMAGE_SYMBOL) * fileHeader.NumberOfSymbols) !=
+            static_cast<qint64>(sizeof(IMAGE_SYMBOL) * fileHeader.NumberOfSymbols))
+        {
+            ncWarning("Cannot read the symbol table.");
+            return;
+        }
+
+        uint32_t stringTableSize;
+        if (source_->read(reinterpret_cast<char *>(&stringTableSize), sizeof(stringTableSize)) != sizeof(stringTableSize)) {
+            ncWarning("Cannot read the size of the string table.");
+            return;
+        }
+
+        std::unique_ptr<char[]> stringTable(new char[stringTableSize]);
+        memset(stringTable.get(), 0, 4);
+        if (source_->read(stringTable.get() + 4, stringTableSize - 4) != stringTableSize - 4) {
+            ncWarning("Cannot read the string table.");
+            return;
+        }
+
+        auto getStringFromTable = [&](uint32_t offset) -> QString {
+            if (offset < stringTableSize) {
+                return QString::fromLatin1(
+                    stringTable.get() + offset, qstrnlen(stringTable.get() + offset, stringTableSize - offset));
+            } else {
+                return QString();
+            }
+        };
+
+        foreach (IMAGE_SYMBOL &symbol, symbols) {
+            peByteOrder.convertFrom(symbol.Type);
+            peByteOrder.convertFrom(symbol.Value);
+
+            using core::image::Symbol;
+
+            Symbol::Type type;
+            /*
+             * Microsoft tools set this field to 0x20 (function) or 0x0 (not a function).
+             * -- http://www.kishorekumar.net/pecoff_v8.1.htm
+             */
+            if (symbol.Type == 0x20) {
+                type = Symbol::FUNCTION;
+            } else {
+                type = Symbol::NOTYPE;
+            }
+
+            QString name;
+            if (symbol.N.Name.Short) {
+                name = getString(symbol.N.ShortName);
+            } else {
+                name = getStringFromTable(symbol.N.Name.Long);
+            }
+
+            image_->addSymbol(std::make_unique<Symbol>(type, name, symbol.Value));
+        }
+
+        foreach (auto section, image_->sections()) {
+            if (section->name().startsWith('/')) {
+                uint32_t offset;
+                if (stringToInt(section->name().mid(1), &offset)) {
+                    QString newName = getStringFromTable(offset);
+                    if (!newName.isEmpty()) {
+                        section->setName(newName);
+                    }
+                }
+            }
+        }
+    }
+
     template<std::size_t size>
     QString getString(const char (&buffer)[size]) const {
         return QString::fromLatin1(buffer, qstrnlen(buffer, size));
-    }
-
-    QString getStringFromTable(uint32_t offset) const {
-        if (offset < stringTableSize_) {
-            return QString::fromLatin1(stringTable_.get() + offset, qstrnlen(stringTable_.get() + offset, stringTableSize_ - offset));
-        } else {
-            return QString();
-        }
     }
 };
 
