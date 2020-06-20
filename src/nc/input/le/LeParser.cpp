smartdec/smartdec

commit 99ebd6ab77db883e973aecabc29258e974f9fc83

Fix some clang-tidy warnings

src/nc/input/le/LeParser.cpp
@@ -54,7 +54,7 @@ boost::optional<HeaderPos> findHeaderPos(QIODevice *in) {
         char le_sig[2];
         if (!read(in, le_sig))
             goto rewind;
-        if (memcmp(le_sig, "LE", 2))
+        if (memcmp(le_sig, "LE", 2) == 0)
             goto rewind;
         HeaderPos hpos = {mz, mz + le_off};
         return hpos;

src/nc/arch/arm/ArmArchitecture.cpp
@@ -43,9 +43,8 @@ ByteOrder ArmArchitecture::getByteOrder(core::ir::Domain domain) const {
         domain == core::ir::MemoryDomain::STACK)
     {
         return byteOrder_;
-    } else {
-        return ByteOrder::LittleEndian;
     }
+    return ByteOrder::LittleEndian;
 }
 
 std::unique_ptr<core::arch::Disassembler> ArmArchitecture::createDisassembler() const {

src/nc/arch/arm/ArmInstructionAnalyzer.cpp
@@ -807,11 +807,11 @@ private:
         unreachable();
     }
 
-    static std::unique_ptr<core::ir::Term> createRegisterAccess(int reg) {
+    static std::unique_ptr<core::ir::Term> createRegisterAccess(unsigned int reg) {
         return ArmInstructionAnalyzer::createTerm(getRegister(reg));
     }
 
-    static const core::arch::Register *getRegister(int reg) {
+    static const core::arch::Register *getRegister(unsigned int reg) {
         switch (reg) {
         #define REG(lowercase, uppercase) \
             case ARM_REG_##uppercase: return ArmRegisters::lowercase();

src/nc/arch/x86/X86Disassembler.cpp
@@ -43,7 +43,7 @@ std::shared_ptr<core::arch::Instruction> X86Disassembler::disassembleSingleInstr
     ud_set_input_buffer(&ud_obj_, const_cast<uint8_t *>(static_cast<const uint8_t *>(buffer)), checked_cast<std::size_t>(size));
 
     SmallByteSize instructionSize = ud_disassemble(&ud_obj_);
-    if (!instructionSize || ud_obj_.mnemonic == UD_Iinvalid) {
+    if (instructionSize == 0 || ud_obj_.mnemonic == UD_Iinvalid) {
         return nullptr;
     }
 

src/nc/core/mangling/DefaultDemangler.cpp
@@ -32,7 +32,7 @@ QString doDemangle(const char *symbol) {
     }
     if (auto output = std::unique_ptr<char[], FreeDeleter>(__unDName(nullptr, symbol, 0, 0))) {
         /* __unDName returns the input string, if fails do demangle. */
-        if (strcmp(output.get(), symbol)) {
+        if (strcmp(output.get(), symbol) != 0) {
             return QLatin1String(output.get());
         }
     }

commit aba7afee7f2c7ee87560f9db90de30f6db84d5b3

Fix regression introduced in 99ebd6ab77db883e973aecabc29258e974f9fc83

src/nc/input/le/LeParser.cpp
@@ -54,7 +54,7 @@ boost::optional<HeaderPos> findHeaderPos(QIODevice *in) {
         char le_sig[2];
         if (!read(in, le_sig))
             goto rewind;
-        if (memcmp(le_sig, "LE", 2) == 0)
+        if (memcmp(le_sig, "LE", 2) != 0)
             goto rewind;
         HeaderPos hpos = {mz, mz + le_off};
         return hpos;